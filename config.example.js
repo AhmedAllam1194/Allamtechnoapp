515e119e13221e2270ae5ad3b677e0e675a11ac9
AhmedAllam1194 <ahmedallam1194@gmail.com>
2025-08-11 04:41:30 +0300

    Update config.example.js

--- config.example.js
+++ config.example.js
@@ -1,10 +1,9 @@
-// AllamTechnoapp – Firebase config
 window.AT_FIREBASE_CONFIG = {
-  apiKey: "AIzaSyCJdGS3ZQ4-ZWsR--m_B4SuifzVKP3kXFY",
+  apiKey: "AIzaSyCJdGS3ZQ4-ZWsR-_m_B45uifzVKP3kXFY",
   authDomain: "allamtechnoapp.firebaseapp.com",
   projectId: "allamtechnoapp",
-  storageBucket: "allamtechnoapp.appspot.com",
-  messagingSenderId: "213906724911",
-  appId: "1:213906724911:web:7cfa075761648b82876fa2",
-  measurementId: "G-TZDE8SHYE0"
+  storageBucket: "allamtechnoapp.firebasestorage.app",
+  messagingSenderId: "213007674911",
+  appId: "1:213007674911:web:27caf05761648b82876f2a",
+  measurementId: "G-TZ0BESHYE0"
 };
